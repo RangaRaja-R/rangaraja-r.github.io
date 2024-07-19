488cd699e965b9b2abefe21e9bc4d32fe212b363
Ranga Raja R <r34rangaraja@gmail.com>
2024-07-19 20:06:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/next-portfolio' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/next-portfolio/' : '',
+    output: 'export'
 };
 
 export default nextConfig;
